e753b2915022f50bc7f4a3986531f233955f2e08
Peder Korsveien <peder.korsveien@gmail.com>
2016-01-22 21:20:03 +0100

    Switch to tomorrow-night theme

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 		      cider
 		      projectile
 		      rainbow-delimiters
-		      noctilux-theme
+		      color-theme-sanityinc-tomorrow
 		      smex))
 
 (when (not package-archive-contents)
@@ -24,6 +24,13 @@
 (require 'config-general)
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(menu-bar-mode nil)
@@ -40,4 +47,10 @@
  '(tool-bar-mode nil)
  '(x-select-enable-clipboard t))
 
-(load-theme 'noctilux t)
+(load-theme 'sanityinc-tomorrow-night t)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
